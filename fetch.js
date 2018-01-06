88cfc89b688bee7ce98439ca275dcd8dced52748
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2018-01-06 02:27:53 -0500

    Working pull + ES save

--- fetch.js
+++ fetch.js
@@ -1,3 +1,12 @@
+// Fetches data about hearings in the US Congress from gpo.gov (GovInfo).
+// The hearing data is dumped into an Elasticsearch instance.
+
+// Modify these constants as needed.
+
+const FIRST_YEAR = 1993;
+const LAST_YEAR = 2017;
+const REQUESTS_PER_SECOND = 5;
+
 const { promisify } = require('util');
 const _ = require('lodash');
 const parseXmlString = promisify(require('xml2js').parseString);
@@ -5,28 +14,32 @@ const request = promisify(require('request'));
 const textVersion = require('textversionjs');
 const PromiseThrottle = require('promise-throttle');
 const elasticsearch = require('elasticsearch');
-
-const FIRST_YEAR = 1993;
-const LAST_YEAR = 2017;
+const ProgressBar = require('progress');
 
 const GPO_ROOT = 'https://www.gpo.gov';
 const gpoThrottle = new PromiseThrottle({
-    requestsPerSecond: 30,
+    requestsPerSecond: REQUESTS_PER_SECOND,
     promiseImplementation: Promise
 });
 
 const esClient = new elasticsearch.Client({
     host: process.env.ELASTIC_HOST,
-    // log: 'trace'
+    httpAuth: `${process.env.ELASTIC_USER}:${process.env.ELASTIC_PASSWORD}`
 });
 
+/**
+ * Fetches a hearing minutes page and returns a plaintext version of it. The
+ * request is subject to the global request throttle.
+ */ 
 async function fetchHearingText(contentUrl) {
-    // console.log('A', contentUrl);
     const response = await gpoThrottle.add(() => request({ url: contentUrl }));
-    // console.log('B', response.body.length, contentUrl);
     return textVersion(response.body);
 }
 
+/**
+ * Fetches an XML document and returns a JSON representation of it. The request
+ * is subject to the global request throttle.
+ */ 
 async function fetchXml(url) {
     const response = await gpoThrottle.add(() => request({ url }));
     try {
@@ -37,7 +50,22 @@ async function fetchXml(url) {
     }
 }
 
-async function fetchHearing(detailUrl) {
+/**
+ * Determines whether a hearing id already exists in Elasticsearch.
+ */
+async function hearingExists(elasticId) {
+    return await esClient.exists({
+        index: 'hearings',
+        type: 'all',
+        id: elasticId
+    });
+}
+
+/**
+ * Fetches the details for a hearing and adds it to Elasticsearch (if necessary).
+ */
+async function fetchHearing(detailUrl, checkedExists=false) {
+    // Get the data from the MODS file
     const modsUrl = detailUrl.replace('content-detail.html', 'mods.xml');
     const data = await fetchXml(modsUrl);
     const allExtensions = _.get(data, 'mods.extension');
@@ -47,6 +75,7 @@ async function fetchHearing(detailUrl) {
     }
     const extensions = Object.assign({}, ...allExtensions);
 
+    // Extract desired attributes
     var committeeThomasId = _.get(extensions, 'congCommittee[0].$.authorityId');
     if (committeeThomasId) {
         committeeThomasId = committeeThomasId.substr(0, 4);
@@ -71,24 +100,26 @@ async function fetchHearing(detailUrl) {
     const isNomination = _.get(extensions, 'isNomination[0]') === 'true';
     const isErrata = _.get(extensions, 'isErrata[0]') === 'true';
 
+    // Abort if there is insufficient identification information for this hearing
+    if (!_.every([congressChamber, congressNumber, jacketId])) {
+        return;
+    }
+
+    // Abort if the hearing already exists in Elasticsearch
     const elasticId =
         (congressChamber === 'house' ? 'H' : 'S')
         + '-' + congressNumber + '-' + jacketId;
-    if (
-        await esClient.exists({
-            index: 'hearings',
-            type: 'all',
-            id: elasticId
-        })
-    ) {
+    if (!checkedExists && await hearingExists(elasticId)) {
         return;
     }
     
+    // Fetch the minutes
     const pathComponents = detailUrl.split('/');
     const pageId = pathComponents[pathComponents.length - 2];
     const contentUrl = `${GPO_ROOT}/fdsys/pkg/${pageId}/html/${pageId}.htm`;
     const content = await fetchHearingText(contentUrl);
 
+    // Build the hearing object
     const hearing = {
         committeeThomasId,
         subcommitteeName,
@@ -104,7 +135,7 @@ async function fetchHearing(detailUrl) {
         content
     };
 
-    // console.log('pre', elasticId);
+    // Add the hearing to Elasticsearch
     try {
         await esClient.create({
             index: 'hearings',
@@ -116,17 +147,64 @@ async function fetchHearing(detailUrl) {
         console.log(err);
         throw err;
     }
-    // console.log('post', elasticId);
 }
 
+/**
+ * Fetches the details for a hearing and adds it to Elasticsearch (if necessary).
+ * Also does a preliminary check to see if the hearing already exists in
+ * Elasticsearch.
+ */
+async function fetchHearingWithCache(detailUrl) {
+    const pathComponents = detailUrl.split('/');
+    const pageId = pathComponents[pathComponents.length - 2];
+    const pageIdComponents = pageId.match(/CHRG\-(\d{3})([hs])hrg(\d{2})(\d{3})/);
+    if (pageIdComponents) {
+        [_, number, chamber, jacket1, jacket2] = pageIdComponents;
+        if (await hearingExists(`${chamber.toUpperCase()}-${number}-${jacket1}-${jacket2}`)) {
+            return;
+        }
+        fetchHearing(detailUrl, true);
+        return;
+    }
+    fetchHearing(detailUrl);
+    return;
+}
+
+/**
+ * Returns a function that fetches a hearing, adds it to Elasticsearch, and
+ * updates a progress bar when it is done.
+ */
+function fetchHearingWithProgress(bar) {
+    return async (detailUrl) => {
+        await fetchHearing(detailUrl);
+        bar.tick(1);
+    };
+}
+
+/**
+ * Fetches all the hearings in the given year and adds them to Elasticsearch
+ * while displaying a progress bar.
+ */
 async function fetchHearingsInYear(year) {
     const data = await fetchXml(
         `${GPO_ROOT}/smap/fdsys/sitemap_${year}/${year}_CHRG_sitemap.xml`
     );
     const urls = _.compact(data.urlset.url.map(url => _.get(url, 'loc[0]')));
-    await Promise.all(urls.map(fetchHearing));
+    const bar = new ProgressBar(
+        `${year} [:bar] :rate/s :current/:total :etas`,
+        {
+            complete: '=',
+            incomplete: ' ',
+            width: 40,
+            total: urls.length
+        }
+    );
+    await Promise.all(urls.map(fetchHearingWithProgress(bar)));
 }
 
+/**
+ * Fetches all the hearings in the year range and adds them to Elasticsearch.
+ */
 async function fetchHearings() {
     const years = _.range(FIRST_YEAR, LAST_YEAR + 1);
     for (const year of years) {
@@ -134,4 +212,5 @@ async function fetchHearings() {
     }
 }
 
+// Entry point
 fetchHearings();
